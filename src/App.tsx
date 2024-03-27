a2e8ea599264a8f8bd63cefd9b18c7712ac9937a
GIORGISHALAMBER\G.Shalamberidze <g.shalamberidze@tnet.ge>
2024-03-27 17:03:10 +0400

    draw map section

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import MapSection from "./components/MapSection";
 
 function App() {
   const { t, i18n } = useTranslation();
 
   return (
-    <div className="">
+    <div className="w-full">
       <Header />
       <Hero />
+      <MapSection />
     </div>
   );
 }
